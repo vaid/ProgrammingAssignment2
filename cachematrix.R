d67ad2bf2186676d2594f55876da18cec9dea9a9
PIYUSH VAID <vaidpiyush.30@gmail.com>
2018-11-24 18:43:48 +0530

    Written my logic of required functions
    Written logic of makeCacheMatrix() and cacheSolve() and also shown an example usage

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Inversing a big matrix is time-consuming process
+## Below functions are used to calulate inverse of matrix just once
+## thus avoiding repeated calculations.
 
-## Write a short comment describing this function
+## Clear the environment
+rm(list = ls())
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
-
-
-## Write a short comment describing this function
-
+## cacheSolve takes the list(returned by makeCacheMatrix) and 
+## returns mean of matrix
+## The mean is calculated first time and cached using setInverse()
+## Next time the value is fetched from getInverse()
+## thus saving time
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
+
+
+## sample example
+##    Create a big matrix
+##    x is a matrix of dimension 2000 X 2000
+x <- matrix(ceiling(runif(4000000, -4000000, 4000000)),2000,2000)
+
+##    makeCacheMatrix takes x(the matrix) and returns a list of functions
+f <- makeCacheMatrix(x)
+
+##    cacheSolve takes the list returned by makeCacheMatrix 
+##    and returns mean
+##    first time executing takes time, as mean is actually 
+##    calculated and cached
+g <- cacheSolve(f)
+
+##    next time you ask to return mean of the matrix 
+##    using cachemean it instanly return the result
+g <- cacheSolve(f)
